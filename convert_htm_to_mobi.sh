33c371a78b1bf99b3a3fd50afcf7d77bbf52d66b
lukereding <lukereding@gmail.com>
2016-04-27 16:14:11 -0500

    update lab notes

--- convert_htm_to_mobi.sh
+++ convert_htm_to_mobi.sh
@@ -2,13 +2,15 @@
 
 PATH="$1"
 
-for file in $PATH*.htm
+for file in $PATH*.epub
 do
     printf "processing $file"
     # get filename without extension
-    f=${file%.htm}
+    f=${file%.epub}
     f_=${f##*/}
     printf $f_
     # do the conversion
     /Users/lukereding/Downloads/KindleGen_Mac_i386_v2_9/kindlegen $file -verbose -o $f_.mobi
+    # copy onto kindle
+    # mv "{$PATH}/{$f_}.mobi" "/Volumes/Kindle/documents/" # not tested
 done
