f5758e037bc0a68cf43346a0e13f5c8caa3b8c9f
luke reding <lukereding@gmail.com>
2017-10-03 16:59:18 -0500

    add effect size conversions

--- plotting_functions.R
+++ plotting_functions.R
@@ -11,6 +11,82 @@ require(tidyverse)
 require(viridis)
 require(RColorBrewer)
 
+
+# Functions for converting between effect sizes ===================
+
+# make sure arguments are of the correct type and not NULL
+check_args <- function(x, type){
+  if (!is.null(x) && class(x) == type)
+    invisible()
+  else
+    stop(paste0("incorrect type or null argument: ", x))
+}
+
+
+d_to_r <- function(d = NULL, n1 = NULL, n2 = NULL) {
+  
+  # based on https://www.meta-analysis.com/downloads/Meta-analysis%20Converting%20among%20effect%20sizes.pdf
+  
+  # check arguments
+  args <- as.list(match.call())
+  missing_args <- setdiff(c("d", "n1", "n2"), names(args))
+  if (length(missing_args) > 1) {
+    stop(paste0("missing arguments "), paste0(missing_args, collapse = ", "))
+  }
+  
+  # mapply(check_args, args[2:length(args)], rep("numeric", length(args) - 1))
+  
+  a <- ((n1 + n2)^2) / (n1 * n2)
+  
+  d / sqrt((d^2) + a)
+  
+}
+
+logodds_to_d <- function(log_odds = NULL) {
+  
+  # based on https://www.meta-analysis.com/downloads/Meta-analysis%20Converting%20among%20effect%20sizes.pdf
+  
+  # check arguments
+  args <- as.list(match.call())
+  missing_args <- setdiff(c("log_odds"), names(args))
+  
+  if (length(missing_args) > 1) {
+    stop(paste0("missing arguments "), paste0(missing_args, collapse = ", "))
+  }
+  
+  # mapply(check_args, args[2:length(args)], rep("numeric", length(args) - 1))
+  
+  log_odds * sqrt(3) / pi
+  
+}
+
+r_to_d <- function(r = NULL) {
+  
+  # based on https://www.meta-analysis.com/downloads/Meta-analysis%20Converting%20among%20effect%20sizes.pdf
+  
+  # check arguments
+  args <- as.list(match.call())
+  missing_args <- setdiff(c("r"), names(args))
+  
+  if (length(missing_args) > 1) {
+    stop(paste0("missing arguments "), paste0(missing_args, collapse = ", "))
+  }
+  
+  if (r > 1L | r < -1L) {
+    stop("the correlation coefficent must be in the range [-1, 1]")
+  }
+  
+  # mapply(check_args, args[2:length(args)], rep("numeric", length(args) - 1))
+  
+  (2 * r) / sqrt(1 - (r^2))
+  
+}
+
+
+
+
+
+
 # for exploratory data analysis
 
 plot_histograms <- function(data){
@@ -29,7 +105,7 @@ plot_counts <- function(data){
     gather(variable, value) %>%
     ggplot(aes(x = value)) +
     geom_bar() +
-    facet_wrap(~variable, scale = "free_x")+
+    facet_wrap(~variable, scale = "free")+
     theme_minimal()
 }
 
@@ -49,7 +125,6 @@ plot_cat_relationship <- function(data, categorical_variable) {
     ggtitle(paste0("relationships with ", quo_name(enquo_cat))) +
     theme_minimal()
 }
-
 
 # theme for ggplot
 theme_mod <- function(font_size = 12, font_family = "", line_size = .5) {
