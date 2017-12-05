8001e30ee0117f8106295f5be8b3f308217fb384
luke reding <lreding@continuum.io>
2017-12-05 15:44:10 -0600

    updates

--- eta.R
+++ eta.R
@@ -31,54 +31,77 @@ eda <- function(x) {
         var_2_type <- find_type(x[,j])
         #print(paste("var1 type: ", var_1_type, "\nvar2 type: ", var_2_type, "\n\n"))
         
+        x1 <- x[,i]
+        x2 <- x[,j]
+        
+        # remove NAs for simplicity
+        if(any(is.na(x1))){
+          # get NA indicies
+          ind <- which(is.na(x1))
+          x1 <- x1[-ind]
+          x2 <- x2[-ind]
+        }
+        
+        if(any(is.na(x2))){
+          # get NA indicies
+          ind <- which(is.na(x2))
+          x1 <- x1[-ind]
+          x2 <- x2[-ind]
+        }
+        
+        # make sure x1 and x2 are the same length
+        stopifnot(length(x1) == length(x2))
+        
+        n <- length(x1)
+        
         if(var_1_type == "numeric" & var_2_type == "numeric") {
           # run a correlation
-          result <- cor.test(x[,i], x[,j])
+          result <- cor.test(x1, x2)
           df <- add_row(df, 
                   var1 = names(x)[i],
                   var2 = names(x)[j],
                   statistic = "r",
                   value = result$estimate,
                   p_value = result$p.value,
-                  n = NA_integer_
+                  n = n
                   )
         } else if(var_1_type == "factor" & var_2_type == "numeric") {
           # run an ANOVA or t-test, depending on number of levels
-          num_levels <- length(levels(x[,i]))
+          num_levels <- length(levels(x1))
           require(ICC)
-          result <- ICCest(names(x)[i], names(x)[j], data = x)$ICC
+          result <- ICCest(x1, x2)$ICC
           df <- add_row(df, 
                         var1 = names(x)[i],
                         var2 = names(x)[j],
                         statistic = "ICC",
                         value = result,
                         p_value = NA_real_,
-                        n = NA_integer_
+                        n = n
           )
         } else if(var_1_type == "numeric" & var_2_type == "factor") {
           # run an ANOVA or t-test, depending on number of levels
-          num_levels <- length(levels(x[,j]))
+          num_levels <- length(levels(x2))
           require(ICC)
-          result <- ICCest(names(x)[j], names(x)[i], data = x)$ICC
+          result <- ICCest(x2, x1)$ICC
           df <- add_row(df, 
                         var1 = names(x)[i],
                         var2 = names(x)[j],
                         statistic = "ICC",
                         value = result,
                         p_value = NA_real_,
-                        n = NA_integer_
+                        n = n
           )
         } else if(var_1_type == "factor" & var_2_type == "factor") {
           require("GoodmanKruskal")
             # compute the GKtau statistic
-          stat <- GKtau(x[,i], x[,j])$tauxy
+          stat <- GKtau(x1, x2)$tauxy
           df <- add_row(df, 
                         var1 = names(x)[i],
                         var2 = names(x)[j],
                         statistic = "tau",
                         value = stat,
                         p_value = NA_real_,
-                        n = NA_integer_
+                        n = n
           )
         } else{
             # return an empty row
@@ -88,19 +111,17 @@ eda <- function(x) {
                         statistic = NA_character_,
                         value = NA_integer_,
                         p_value = NA_real_,
-                        n = NA_integer_
+                        n = n
           )
           }
       }
     }
-  arrange(df[-1,], p_value)
+  df[-1,]
 }
-
-iris$interval <- cut(iris$Sepal.Length, 4)
 
 eda(iris)
 
-eda(df) %>% 
+eda(iris) %>% 
   filter(!is.na(value)) %>%
   unite(variables, var1, var2, sep = " :: ") %>%
   ggplot(aes(y = value, x = reorder(variables, value))) +
@@ -108,5 +129,5 @@ eda(df) %>%
   coord_flip() +
   facet_wrap(~statistic, scales = "free") +
   theme_minimal()
-ggsave("~/Desktop/out.pdf", width = 20, height = 15)
+# ggsave("~/Desktop/out.pdf", width = 20, height = 15)
 
