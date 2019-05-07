e3b78eff8e815a813573c5803f91c77521ea2cd8
luke reding <lukereding@gmail.com>
2019-05-07 06:33:26 -0400

    Update web_visit.py

--- web_visit.py
+++ web_visit.py
@@ -16,10 +16,11 @@ docker run -it -v $(pwd):/opt mmast/scrapy-tor python web_visit.py
 ua = UserAgent()
 
 url = 'http://journals.plos.org/plosntds/article?id=10.1371/journal.pntd.0001969'
+url = "https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1008125"
 
 i = 0
 
-while i < 200:
+while True:
 
     header = {'User-Agent' : ua.random}
 
